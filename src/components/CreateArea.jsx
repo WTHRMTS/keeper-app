a31bcbeb25e6b8e0585ef7f07477c58f6a275b00
Jamie Booth <Malediction99@gmail.com>
2021-10-08 14:18:06 +1100

    finalish

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,12 +1,40 @@
-import React from "react";
+import React, {useState} from "react";
 
-function CreateArea() {
+function CreateArea(props) {
+  const [note, setNote] = useState({
+    noteTitle: "",
+    noteContent: "",
+  });
+  function handleChange(event) {
+    const {value, name} = event.target
+    setNote(prevValue => {
+      if (name === "title") {
+        return {
+          noteTitle: value,
+          noteContent: prevValue.noteContent
+        };
+      } else if (name === "content") {
+        return {
+          noteTitle: prevValue.noteTitle,
+          noteContent: value
+        };
+      }
+    })
+  }
+  
   return (
     <div>
       <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
-        <button>Add</button>
+        <input onChange={handleChange} name="title" placeholder="Title" value={note.noteTitle} />
+        <textarea onChange={handleChange} name="content" placeholder="Take a note..." rows="3" value={note.noteContent}/>
+        <button onClick={(event)=> {
+          props.onAdd(note);
+          setNote({
+              noteTitle: "",
+              noteContent: "",
+            })
+            event.preventDefault();
+        }}>Add</button>
       </form>
     </div>
   );
